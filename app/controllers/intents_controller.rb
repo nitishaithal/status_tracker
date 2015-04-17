b8c3ce7239b5f958ca5e9279281401548ce1f09e
nitishaithal <nitish.k008@gmail.com>
2015-04-17 13:10:37 +0530

    delete functionality for intent and milestones

--- app/controllers/intents_controller.rb
+++ app/controllers/intents_controller.rb
@@ -1,9 +1,11 @@
 class IntentsController < ApplicationController
+before_action :find_intent, only: [:destroy]
+
 def create
 	@intent = Intent.new(intent_params)
 	#if the goal is able to save then redirect to the goals page
 	if @intent.save
-		redirect_to @intent
+		redirect_to @intent.milestone
 	else 
 		render 'new'
 	end
@@ -23,10 +25,17 @@ def update
 			render 'edit'
 		end
 	end
-
+def destroy
+		@intent.destroy
+		redirect_to @intent.milestone
+end
 	private
 
 	def intent_params
 		params.require(:intent).permit(:description, :status_intent, :milestone_id)
+	end
+
+	def find_intent
+		@intent = Intent.find(params[:id])
 	end
 end
--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -1,10 +1,11 @@
 class MilestonesController < ApplicationController
+before_action :find_milestone, only: [:destroy]
 
 def create
 	@milestone = Milestone.new(milestone_params)
 	#if the goal is able to save then redirect to the goals page
 	if @milestone.save
-		redirect_to @milestone
+		redirect_to @milestone.goal
 	else 
 		render 'new'
 	end
@@ -18,7 +19,7 @@ def show
 	@milestones = Milestone.all
 	@intents = Intent.all
 	@milestone = Milestone.find(params[:id])
-	redirect_to @milestone.goal
+	#redirect_to @milestone.goal
 end
 def update
 	@milestone = Milestone.find(params[:id])
@@ -29,9 +30,18 @@ def update
 	    format.json { render :json => @milestone.to_json }
 	end
 end
+
+def destroy
+	@milestone.destroy
+	redirect_to @milestone.goal
+end
 	private
 
 	def milestone_params
 		params.require(:milestone).permit(:description, :status_milestone, :est_time_milestone, :goal_id)
+	end
+
+	def find_milestone
+		@milestone = Milestone.find(params[:id])
 	end
 end
