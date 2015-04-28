55e4f074eb36dd2808a83e9dc04892b1859ceac1
nitishaithal <nitish.k008@gmail.com>
2015-04-29 01:03:05 +0530

    chat with pubnub

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,8 +1,32 @@
 class ChatController < ApplicationController
  
   def start_conversation
-  	@usr=User.find(params[:id])
+  	@user=User.find(params[:id])
+  	@key = " "
+  	@message ="+"
+  	if (@user.email  <=> current_user.email) < 0
+  		@key = @user.email+"_@_"+current_user.email
+  	else
+  		@key = current_user.email+"_@_"+@user.email
+  	end 
+  	
+  	@messages = $redis.get(@key)
+
   	render template: "projects/chat"
+
   end
+
+def sto
+	require "redis"
+	redis = Redis.new
+	keyis = params[:@key]
+	newmsg = params[:message]
+	redis.append(keyis, newmsg);
+	redirect_to root_path
+
+	#value = redis.get('foo');
+	#puts value
+  end
+
 
 end
--- app/helpers/chat_helper.rb
+++ app/helpers/chat_helper.rb
@@ -1,2 +1,16 @@
 module ChatHelper
+
+
+def sto
+	require "redis"
+	redis = Redis.new
+	keyis = "key"
+	newmsg = params[:message]
+	redis.append(keyis, newmsg);
+	redirect_to root_path
+
+	#value = redis.get('foo');
+	#puts value
+  end
+
 end
